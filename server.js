9a04471257378b6bf48dfa014a219bcec39d7cf5
aubbiali <aubbiali@sorint.it>
2022-08-23 11:39:05 +0200

    added env file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.listen(3000);
-console.log('Server started at http://localhost:' + 3000);
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
