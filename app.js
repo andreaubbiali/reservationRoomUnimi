188e5767892c565840798fdb7eac70840ad60e11
aubbiali <aubbiali@sorint.it>
2022-08-26 10:16:22 +0200

    redirect to login

--- app.js
+++ app.js
@@ -5,20 +5,21 @@ const app = express();
 require("dotenv").config();
 require("./config/database").connect();
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'static')));
+app.set('views', path.join(__dirname, '/public/views'));
+app.use(express.static('public', {index:'/views/login.html'}));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'static')));
 
 
 const authenticationController = require('./controller/authenticationController');
-app.post('/login', authenticationController.login);
-app.post('/register', authenticationController.register);
+app.post('/api/login', authenticationController.login);
+app.post('/api/register', authenticationController.register);
 
 const birds = require('./router/bird');
-app.use('/birds', birds);
+app.use('/api/birds', birds);
 
-app.use(express.static('public'))
 
 module.exports = app;
--- middleware/auth.js
+++ middleware/auth.js
@@ -4,27 +4,19 @@ const config = process.env;
 
 const verifyToken = (req, res, next) => {
 
-    // if (req.session.loggedin) {
-    //     next();
-    // } else {
-    //     let resp = {
-    //         "response": null
-    //     }
-
-    //     res.render('login', resp);
-    // }
-
     const token =
         req.body.token || req.query.token || req.headers["x-access-token"];
 
     if (!token) {
-        return res.status(403).send("A token is required for authentication");
+        res.redirect('http://localhost:3000/views/login.html');
+        return res.end();
     }
     try {
         const decoded = jwt.verify(token, config.TOKEN_KEY);
         req.user = decoded;
     } catch (err) {
-        return res.status(401).send("Invalid Token");
+        res.status(401).send("Invalid Token");
+        return res.end();
     }
     return next();
 }
