e55ae45bd92572c0e3f619dcbbd2ebfb52c5ff8f
aubbiali <aubbiali@sorint.it>
2022-08-28 16:26:22 +0200

    code improvement

--- model/user.js
+++ model/user.js
@@ -1,11 +1,32 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    firstName: { type: String, trim: true, required:true },
-    lastName: { type: String, default: null, trim: true, required:true },
-    email: { type: String, unique: true, lowercase: true, trim: true, required:true },
-    password: { type: String, required:true },
-    roles: { type: [String], required:true },
+    firstName: { 
+        type: String, 
+        trim: true, 
+        required:true 
+    },
+    lastName: { 
+        type: String, 
+        default: null, 
+        trim: true, 
+        required:true 
+    },
+    email: { 
+        type: String, 
+        unique: true, 
+        lowercase: true, 
+        trim: true, 
+        required:true 
+    },
+    password: { 
+        type: String, 
+        required:true 
+    },
+    roles: {
+        type: [String], 
+        required:true 
+    },
 },
 {versionKey: false});
 
