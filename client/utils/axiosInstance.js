5434edfe97db87473c7e88223c1058344801f22e
aubbiali <aubbiali@sorint.it>
2022-08-31 11:35:20 +0200

    delete code

--- client/utils/axiosInstance.js
+++ client/utils/axiosInstance.js
@@ -13,26 +13,3 @@ exports.axiosRequest = (req, res) =>{
         }
     });
 } 
-
-
-// function setTokenHeaders(token) { 
-
-//     const apiBaseUrl = process.env.API_BASE_URL;
-
-//     const headers = {
-//         'x-access-token': token,
-//     };
-
-//     axiosRequest = axios.create({
-//         baseURL: apiBaseUrl,
-//         headers,
-//     });
-
-//     console.log('FINE TOKEN');
-
-// }
-
-// module.exports = { 
-//     axiosRequest,
-//     setTokenHeaders
-// }
