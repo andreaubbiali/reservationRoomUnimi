d5bfd84f4cf021b9e1c86967b3f637050a071ecf
aubbiali <aubbiali@sorint.it>
2022-08-31 09:53:34 +0200

    format code

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const http = require("http");
  * Start server
  */
 
- const serverApp = require("./server/app");
- const server = http.createServer(serverApp);
- const serverPort = process.env.API_PORT;
- 
- // server listening 
- server.listen(serverPort, () => {
-   console.log(`Server running on port ${serverPort}`);
- });
+const serverApp = require("./server/app");
+const server = http.createServer(serverApp);
+const serverPort = process.env.API_PORT;
+
+// server listening 
+server.listen(serverPort, () => {
+    console.log(`Server running on port ${serverPort}`);
+});
 
 /**
  * Start client
@@ -23,5 +23,5 @@ const clientPort = process.env.CLIENT_PORT;
 
 // server listening 
 client.listen(clientPort, () => {
-  console.log(`Client running on port ${clientPort}`);
+    console.log(`Client running on port ${clientPort}`);
 });
