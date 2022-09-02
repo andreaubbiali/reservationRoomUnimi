682eea1b763958d5334719c5920faebe92e0faf2
aubbiali <aubbiali@sorint.it>
2022-09-02 11:48:25 +0200

    fix error message validation mdlw

--- server/middleware/validationMiddleware.js
+++ server/middleware/validationMiddleware.js
@@ -9,8 +9,14 @@ const { validationResult } = require('express-validator');
 const validationMiddleware = (req, res, next) => {
 
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        res.status(400).json({ errors: errors.array() });
+    if (!errors.isEmpty()) {        
+
+        let errResponse = "";
+        errors.array().forEach(function (err) {
+            errResponse += err.msg + " ";
+        })
+
+        res.status(400).json(errResponse);
         return res.end();
     }
     
