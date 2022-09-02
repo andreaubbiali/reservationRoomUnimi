ade94415eb6067bb26f5aab447386464ea16f58e
aubbiali <aubbiali@sorint.it>
2022-09-02 08:08:44 +0200

    serve public statically

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const http = require("http");
+const http = require('http');
+const express = require('express');
 
 /**
  * Start server
  */
 
-const serverApp = require("./server/app");
+const serverApp = require('./server/app');
 const server = http.createServer(serverApp);
 const serverPort = process.env.API_PORT;
 
@@ -17,7 +18,8 @@ server.listen(serverPort, () => {
  * Start client
  */
 
-const clientApp = require("./client/app");
+const clientApp = require('./client/app');
+clientApp.use(express.static('public'));
 const client = http.createServer(clientApp);
 const clientPort = process.env.CLIENT_PORT;
 
