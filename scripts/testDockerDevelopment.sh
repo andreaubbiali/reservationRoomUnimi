92957ef1bbbc62edfa4cb4240b3dde12b6d42462
andrea <uba99a@gmail.com>
2022-09-15 16:20:39 +0200

    update script

--- scripts/testDockerDevelopment.sh
+++ scripts/testDockerDevelopment.sh
@@ -5,7 +5,6 @@ NC='\033[0m' # No Color
 
 printf "${GREEN}start development${NC}\n"
 
-
 printf "docker app id:"
 docker run -d -t --net=host --name=reservationRoomApp reservationroom
 printf "\n"
@@ -14,13 +13,9 @@ printf "docker db id:"
 docker run -d -t --net=host --name=reservationRoomDB -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=admin mongo 
 printf "\n"
 
-# printf "add records in db \n"
-# cat <<EOF | docker exec reservationRoomDB bash 
-# mongosh -u admin -u admin
-
-# EOF
-
 sleep 2s
+
+exec ./scripts/loadAdminData.sh &
 
 # START APP
 docker exec reservationRoomApp npm run dev
